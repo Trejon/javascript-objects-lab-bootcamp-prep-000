246f0bec70d989ebf5c106ca5c1e92331fd05d7f
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-19 17:48:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var clone = object.assign({},object);
+  delete clone[key];
+  return clone; 
 }
